d2adc59577a4fddc6c75c010ee732477eca72794
creslin <34645187+creslinux@users.noreply.github.com>
2019-04-11 09:05:43 +0000

    Update init_and_run.sh

--- init_and_run.sh
+++ init_and_run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 ## Pre-Script to supervisord should any docker internals massaging be desired
+
+# Check if user wants to run anything before. If file pre_start.sh exists, run this. 
+sleep 2
+if [ -f /pre_start.sh ]; then
+   chmod +x /pre_start.sh
+   bash /pre_start.sh
+
 chown -R nobody:nogroup /apps/
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
