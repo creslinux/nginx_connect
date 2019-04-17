2b720e71c5a12b331a464d060b1f22b258bdc962
creslin <34645187+creslinux@users.noreply.github.com>
2019-04-17 07:56:18 +0000

    Update init_and_run.sh

--- init_and_run.sh
+++ init_and_run.sh
@@ -2,16 +2,14 @@
 ## Pre-Script to supervisord should any docker internals massaging be desired
 
 # Check if user wants to run anything before. If file pre_start.sh exists, run this.
-sleep 2
+echo "Startig up `date`"
+sleep 20 # Let other back end services startup 
+date
+
 if [ -f /pre_start.sh ]; then
    chmod +x /pre_start.sh
    bash /pre_start.sh
 fi
-
-# if there is not a nginx.conf at this point, move the template file to it. 
-if [ ! -f /etc/nginx/nginx.conf ]; then
-  cp /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf
-fi 
 
 chown -R nobody:nogroup /apps/
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
